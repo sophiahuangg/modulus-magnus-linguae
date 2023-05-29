71743156841bbfab1612f133cc40b2093b3b56d6
Sophia Huang <shuang24@students.claremontmckenna.edu>
2023-05-29 01:53:39 -0700

    Adding Shell Script

--- task2/create_json.sh
+++ task2/create_json.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-for pensvm in /home/Sophia.Huang.24/modulus-magnus-linguae/prompts/*
+for pensvm in /home/Sophia.Huang.24/modulus-magnus-linguae/task2/pensvms/*
 do
-    pensvm_folder=$(basename "$pensvm")
-    echo "$pensvm_folder"
-    for prompt in "$pensvm"/*
-    do
-        style=$(basename "$prompt")
-        echo "The Style is: $style"
-        nohup /home/Sophia.Huang.24/modulus-magnus-linguae/task2/extract.py > jsonoutputs/"$pensvm_folder"."$style" &
-    done
+    echo "$pensvm"
+    pensvmName=$(basename "$pensvm")
+    python3 ~/modulus-magnus-linguae/task2/extract.py --pensvmType="$pensvm" &
 done
+
---
+++ task2/extract.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import json, re, argparse, os
+
+def extract_q_and_a(text):
+    question_answer_pattern = r'\|(.*)\|(.*)'
+# Regex patterns to find questions and answers
+    matches = re.findall(question_answer_pattern, text)
+    questions = [match[0] for match in matches]
+    answers = [match[1] for match in matches]
+    return questions, answers
+
+def create_qa_json(questions, answers, pensvmType):
+    filename = os.path.join("jsonoutputs", pensvmType + ".json")
+    qa_pairs = [{"question": q, "answer": a} for q, a in zip(questions, answers)]
+# Creates a dictionary with key "questions" and value as the list of qa_pairs
+    dict_to_dump = {"questions": qa_pairs}
+
+    with open(filename, 'w') as f:
+        json.dump(dict_to_dump, f) 
+# Dumps the newly created dictionary instead of qa_pairs
+
+def main(pensvmType):
+    with open(pensvmType, 'r') as f:
+        text = f.read()
+
+    questions, answers = extract_q_and_a(text)
+    create_qa_json(questions, answers, os.path.basename(pensvmType))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pensvmType')
+    args = parser.parse_args()
+
+    main(args.pensvmType)
