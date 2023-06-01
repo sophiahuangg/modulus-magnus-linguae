915fc8e5d31c668ad1336c2fc6f9d387ce2e3a0a
Sophia Huang <shuang24@students.claremontmckenna.edu>
2023-06-01 15:29:24 -0700

    Adding 35 Chapters and Sorting

--- task2update/json_extract.py
+++ task2update/json_extract.py
@@ -6,38 +6,30 @@ with open(file_path, 'r') as file:
     js_function = file.read()
 
 # Extracting each json for each chapter
-pattern = r'JSON\.parse\((["\'])(.*?)\1\)'
+pattern = r'JSON\.parse\((["\'])(.*?)\1\)}'
 
 matches = re.findall(pattern, js_function, re.DOTALL)
 
 json_objects = []
 
 # Iterates through each json for each chapter
-numerrors = 0
 for match in matches:
-    try:
         unescaped_str = codecs.decode(match[1], 'unicode_escape')
         json_obj = json.loads(unescaped_str)
-        print("json_obj=", json_obj['id'])
         json_objects.append(json_obj)
-    except (json.JSONDecodeError, UnicodeDecodeError):
-        print("JSON ERROR ON=", match[1])
-        numerrors+=1
-        pass
-
-print("NUM ERRORS=", numerrors)
 
 directory = "chapterinfo"
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-# Creates a json file for each chapter
-for chapter in json_objects:
-    #print("Chapter id=", chapter['id'])
-    # Naming as Chapter ID + 1 because ID starts at 0 (i.e. first chapter has id 0, second chapter has id 1, etc.)
-    filename = f"CH_{chapter['id'] + 1}.json"
-    filepath = os.path.join(directory, filename)
+# Sort the chapters
+sort_ch = sorted(json_objects, key = lambda x:int(x['id']))
 
+# Creates a json file for each chapter
+for chapter in sort_ch:
+    # Naming as Chapter ID + 1 because ID starts at 0 (i.e. first chapter has id 0, second chapter has id 1, etc.)
+    filename = f"CH_{str(chapter['id'] + 1).zfill(2)}.json"
+    filepath = os.path.join(directory, filename)
     with open(filepath, 'w') as f:
-        json.dump(chapter, f, indent=4)
+        json.dump(chapter, f, indent = 4)
 
